7c0e1a03cd0992589ff4cf50ecb2212550e7deba
Jonathan Mallett <jonnmallett@gmail.com>
2017-11-04 16:17:45 +0100

    Add project files.

---
+++ BowlingScoreCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingScoreCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
