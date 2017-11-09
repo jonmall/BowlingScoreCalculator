c258e4490e0ddf8bc2e2492a569761d2dfd9ba94
Jonathan Mallett <jonnmallett@gmail.com>
2017-11-09 09:11:53 +0100

    Updated test method

--- BowlingScoreCalculator.Logic.Tests/BowlingCalculatorTests.cs
+++ BowlingScoreCalculator.Logic.Tests/BowlingCalculatorTests.cs
@@ -11,7 +11,7 @@ namespace BowlingScoreCalculator.Logic.Test
         [InlineData("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||", 90)]
         [InlineData("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5", 150)]
         [InlineData("X|7/|9-|X|-8|8/|-6|X|X|X||81", 167)]
-        [InlineData("X|X|9/|X|-8|8/|-/|X|X|X||73", 202)]
+        [InlineData("X|X|9/|X|-8|8/|-/|X|X|X||7-", 199)]
         public void CalculatorResultTest(string input, int result)
         {
             var bowlingCalculator = new BowlingCalculator();
